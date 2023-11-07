83335eb9b3839160dbb761864ad81f5fe2d5b01c
Kyryl Zuiev <102185030+LordWhiskas@users.noreply.github.com>
2023-11-07 14:00:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Backend Developer", "Programmer"],
+    strings: ["ML developer", "Software Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
